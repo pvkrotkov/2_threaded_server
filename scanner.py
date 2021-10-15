29b8add6092c27f2af4ec0b029258dd8dec089fb
Nikita <76069143+wlcoder1@users.noreply.github.com>
2021-10-15 14:34:27 +0300

    Update scanner

--- scanner.py
+++ scanner.py
@@ -1,15 +1,49 @@
-import socket
-from threading import Thread
+import socket, threading
 
-N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
+def TCP_connect(ip, port_number, delay, output):
+    TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    TCPsock.settimeout(delay)
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
+        TCPsock.connect((ip, port_number))
+        output[port_number] = "Слушает"
     except:
-        continue
-    finally:
-        sock.close()
+        output[port_number] = ""
+
+
+def scan_ports(host_ip, delay):
+    # Для одновременного запуска TCP_connect
+    threads = []
+    # Вывод портов
+    output = {}
+
+    # Создание потоков для сканирования портов
+    for i in range(10000):
+        t = threading.Thread(target=TCP_connect, args=(host_ip, i, delay, output))
+        threads.append(t)
+
+    # Начинаем threads
+    for i in range(10000):
+        threads[i].start()
+
+    # Блокировка основного потока до завершения всех потоков
+    for i in range(10000):
+        threads[i].join()
+
+    # Печать портов
+    for i in range(10000):
+        if output[i] == "Слушает":
+            print(str(i) + ": " + output[i])
+
+
+def main():
+    host_ip = input("Напишите ip host: ")
+    delay = int(input("Сколько секунд socket ждет до timeout: "))
+    scan_ports(host_ip, delay)
+
+
+if __name__ == "__main__":
+    main()
+
+# ip = '127.0.0.1'
