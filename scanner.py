4a4195549cbd2c6628cf82fef2e12638a8fbeb0b
obadmaev <80482468+obadmaev@users.noreply.github.com>
2021-10-20 19:09:21 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,19 +1,38 @@
 import socket
-from threading import Thread
+import concurrent.futures
+import tqdm
 
-N = 2**16 - 1
-count_n = N//1000+1
+def scan_port(host, port, progress_bar, ports):
+    sock = socket.socket()  #создаем сокет
+    sock.settimeout(0.5)
+    try:
+        sock.connect((host, port)) #пытаемся приконнектить к порту
+        ports.append(port)
+    except:
+        pass
+    progress_bar.update()
 
-def proc(n):
-    for port in range(n-999,n):
-        sock = socket.socket()
-        try:
-            sock.connect(('127.0.0.1', port))
-            print("Порт", port, "открыт")
-        except:
-            continue
-        finally:
-            sock.close()
 
-threads = [Thread(target = proc, args=[(i+1)*1000]) for i in range(count_n)]
-[t.start() for t in threads]
+host = input('Введите адрес: ')
+start_port = int(input('Введите минимальный порт: '))
+end_port = int(input('Введите максимальный порт: '))
+thread = int(input('Сколько потоков использовать для сканирования одновременно: '))
+
+
+
+open_ports = []     #список открытых портов
+progress_bar = tqdm.tqdm(total=end_port-start_port+1)
+
+with concurrent.futures.ThreadPoolExecutor(thread) as executor: #Выражение with используется для создания исполнительного блока экземпляра ThreadPoolExecutor, который будет быстро очищать потоки после выполнения
+    futures = []
+    for port in range(start_port, end_port+1):
+        future = executor.submit(scan_port, host, port, progress_bar, open_ports) #возвращает объект future из futures
+        futures.append(future)
+    for future in futures:
+        future.result()
+
+progress_bar.close()
+
+
+for port in open_ports:
+    print(f'Порт {port} открыт!')
