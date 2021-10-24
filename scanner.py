101131318f3ccc6bf8943b9900ba549747c1a665
Daniil <kravchenko5690@gmail.com>
2021-10-24 17:29:26 +0300

    done

--- scanner.py
+++ scanner.py
@@ -1,15 +1,31 @@
-import socket
+import socket, time
 from threading import Thread
 
-N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+N = 2**16 - 1
+sock = socket.socket()
+
+
+
+def progress():
+    progress = 0
+    percent = 100 / N
+    for i in range(1, N):
+        progress += percent
+        print('\rОбработка файлов завершена на %3d%%' % progress, end='', flush=True)
+        time.sleep(.00001)
+
+def scann(sock):
+    ip = input('Write the ip >>> ')
+    if ip == '':
+        ip = '127.0.0.1'
+    progress()
+    for port in range(1, N):
+        try:
+            sock.connect((ip, port))
+            time.sleep(.0001)
+        except:
+            print(f'Port {port} is close')
+            continue
+
+Thread(target=scann, args=(sock, )).start()
